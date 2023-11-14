0318e9f857165d7c9f1428f05df51791b0357012
AhmedDR200 <alshwwhy212@gmail.com>
2023-11-14 23:36:43 +0200

    Build Chat Server Administration Models

--- project/settings.py
+++ project/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'account',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -106,3 +109,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "account.Account"
